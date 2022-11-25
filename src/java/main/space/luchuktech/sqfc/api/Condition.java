4b19fd88d95306265a10479f2eb00a6edf0983f6
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-11-24 22:32:14 -0500

    adds docstrings for the Condition interface

--- src/java/main/space/luchuktech/sqfc/api/Condition.java
+++ src/java/main/space/luchuktech/sqfc/api/Condition.java
@@ -1,7 +1,16 @@
 package space.luchuktech.sqfc.api;
 
+/**
+ *
+ * @param <S>
+ */
 public interface Condition<S extends State> {
 
+    /**
+     * Evaluates the condition and returns the result.
+     * @param s The Current state object
+     * @return True or false depending on the result of the evaluation.
+     */
     boolean isTrue(S s);
 
 
