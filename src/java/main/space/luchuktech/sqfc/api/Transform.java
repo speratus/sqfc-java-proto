79047231b653777f348d333be5a7ba39cafeefdd
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-11-24 22:34:33 -0500

    adds docstrings for the Transform interface

--- src/java/main/space/luchuktech/sqfc/api/Transform.java
+++ src/java/main/space/luchuktech/sqfc/api/Transform.java
@@ -1,8 +1,17 @@
 package space.luchuktech.sqfc.api;
 
+/**
+ * A generic interface for transforming a state object.
+ * @param <T>
+ */
 @FunctionalInterface
 public interface Transform<T extends State> {
 
+    /**
+     * Performs some operation on the given state object. Precisely what is determined by the developer.
+     * @param state The current state of the program.
+     * @return The transformed state object.
+     */
     T transform(T state);
 
 }
