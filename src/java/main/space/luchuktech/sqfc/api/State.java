e03fb7b83979cc0dd0c5f353bbd4bb044a614a47
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-11-24 22:30:34 -0500

    adds basic docstrings for State

--- src/java/main/space/luchuktech/sqfc/api/State.java
+++ src/java/main/space/luchuktech/sqfc/api/State.java
@@ -1,10 +1,28 @@
 package space.luchuktech.sqfc.api;
 
+/**
+ *
+ * @param <K>
+ */
 public interface State<K> {
 
 
+    /**
+     * Returns the requested variable. Pass the `returnType` class to ensure that this method returns the value with
+     * the correct type.
+     * @param key The key of the object to return
+     * @param returnType The Expected Class of the return type
+     * @return The value of the requested key. Null if the value doesn't exist.
+     * @param <T> The expected type of the return value.
+     */
     <T> T getVariable(K key, Class<T> returnType);
 
+    /**
+     * Sets a variable of the state.
+     * @param key The key of the object
+     * @param o The value of the state
+     * @return True if the value was successfully set. False, otherwise.
+     */
     boolean setVariable(K key, Object o);
 
 }
