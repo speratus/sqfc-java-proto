f592d803bff6c6bfe2aed63313421153d1f8ff8e
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-11-29 23:43:07 -0500

    adds basic methods for Step

--- src/java/main/space/luchuktech/sqfc/api/Step.java
+++ src/java/main/space/luchuktech/sqfc/api/Step.java
@@ -1,7 +1,17 @@
 package space.luchuktech.sqfc.api;
 
+import java.util.Set;
+
 public interface Step<S extends State<?>, C extends Condition<S>, T extends Transform<S>> {
 
+    Step<S, C, T> nextStep();
 
+    EvaluationResult evaluate(Set<C> conditions, S state);
+
+    S resultingState();
+
+    Set<C> conditions();
+
+    T getTransform();
 
 }
