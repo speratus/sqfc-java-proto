0147bc9633c98bcdac0835b86853ecaff5e42581
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-12-01 18:07:37 -0500

    makes nextStep method return an optional

--- src/java/main/space/luchuktech/sqfc/api/Step.java
+++ src/java/main/space/luchuktech/sqfc/api/Step.java
@@ -1,10 +1,11 @@
 package space.luchuktech.sqfc.api;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface Step<S extends State<?>, C extends Condition<S>, T extends Transform<S>> {
 
-    Step<S, C, T> nextStep();
+    Optional<Step<S, C, T>> nextStep();
 
     EvaluationResult evaluate(Set<C> conditions, S state);
 
